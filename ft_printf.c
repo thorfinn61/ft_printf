1a1d5e7a771cebbbf1ff86b68a6238133f9c55ad
Elyesa Sahin <elsahin@k0r4p7.42mulhouse.fr>
2025-11-11 09:54:27 +0100

    finish

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: elsahin <elsahin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 08:23:49 by elsahin           #+#    #+#             */
-/*   Updated: 2025/11/10 14:44:24 by elsahin          ###   ########.fr       */
+/*   Updated: 2025/11/11 09:18:27 by elsahin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void		handle_format(char c, va_list args, int *count);
+void		handle_format(char c, va_list *args, int *count);
 
 int	ft_printf(const char *format, ...)
 {
@@ -27,7 +27,7 @@ int	ft_printf(const char *format, ...)
 	{
 		if (format[i] == '%' && format[i + 1])
 		{
-			handle_format(format[i + 1], args, &count);
+			handle_format(format[i + 1], &args, &count);
 			i += 2;
 		}
 		else
@@ -41,11 +41,11 @@ int	ft_printf(const char *format, ...)
 	return (count);
 }
 
-static void	handle_pointer(va_list args, int *count)
+static void	handle_pointer(va_list *args, int *count)
 {
 	void	*ptr;
 
-	ptr = va_arg(args, void *);
+	ptr = va_arg(*args, void *);
 	if (!ptr)
 		ft_putstr_fd("(nil)", 1, count);
 	else
@@ -55,23 +55,23 @@ static void	handle_pointer(va_list args, int *count)
 	}
 }
 
-void	handle_format(char c, va_list args, int *count)
+void	handle_format(char c, va_list *args, int *count)
 {
 	if (c == 'c')
-		ft_putchar_fd(va_arg(args, int), 1, count);
+		ft_putchar_fd(va_arg(*args, int), 1, count);
 	else if (c == 's')
-		ft_putstr_fd(va_arg(args, char *), 1, count);
+		ft_putstr_fd(va_arg(*args, char *), 1, count);
 	else if (c == 'd' || c == 'i')
-		ft_putnbr_fd(va_arg(args, int), 1, count);
+		ft_putnbr_fd(va_arg(*args, int), 1, count);
 	else if (c == '%')
 		ft_putchar_fd('%', 1, count);
 	else if (c == 'u')
-		ft_putnbr_base_ul(va_arg(args, unsigned int), "0123456789", count);
+		ft_putnbr_base_ul(va_arg(*args, unsigned int), "0123456789", count);
 	else if (c == 'x')
-		ft_putnbr_base_ul(va_arg(args, unsigned int), "0123456789abcdef",
+		ft_putnbr_base_ul(va_arg(*args, unsigned int), "0123456789abcdef",
 			count);
 	else if (c == 'X')
-		ft_putnbr_base_ul(va_arg(args, unsigned int), "0123456789ABCDEF",
+		ft_putnbr_base_ul(va_arg(*args, unsigned int), "0123456789ABCDEF",
 			count);
 	else if (c == 'p')
 		handle_pointer(args, count);
